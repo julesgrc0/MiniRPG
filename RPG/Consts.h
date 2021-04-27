59347fd3aae4d9bb3e38fd34d1d4592d4f914631
Jules <60539590+JulesG10@users.noreply.github.com>
2021-04-27 20:51:25 +0200

    map.json

--- RPG/Consts.h
+++ RPG/Consts.h
@@ -1,3 +1,3 @@
 #pragma once
-#define MAP_PATH "C:\\Users\\jules\\OneDrive\\Bureau\\map_example.json"
+#define MAP_PATH "C:\\Users\\jules\\source\\repos\\RPG\\x64\\Debug\\assets\\Maps\\map.json"
 #define FONT_PATH "C:\\Users\\jules\\source\\repos\\RPG\\x64\\Debug\\assets\\Fonts\\Arial\\arial.ttf"
